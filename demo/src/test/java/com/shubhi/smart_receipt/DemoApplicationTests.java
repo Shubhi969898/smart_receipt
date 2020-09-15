6c123c741ccf947352845f7c3432a5eb898e15ae
Hp <Hp@SHUBHI-PC>
2020-09-15 14:47:42 +0530

    initial

---
+++ demo/src/test/java/com/shubhi/smart_receipt/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubhi.smart_receipt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
